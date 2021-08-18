59ab5b8dc8349accfd3226938e99ef2adff5162c
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-19 00:40:25 +0800

    Delete public directory

--- public/result.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("shortUrl").value = sessionStorage.getItem("shortUrl"); 
-
-function copy(id) {
-    var copyText = document.getElementById(id);
-    copyText.select();
-    copyText.setSelectionRange(0, 99999);
-    document.execCommand("copy");
-}
-
